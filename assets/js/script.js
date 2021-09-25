987f1cfe609d59dbb2b1cb9b3358e31fef898290
Cole Enyart <letchro@gmail.com>
2021-09-24 22:51:48 -0500

    added more questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,41 +36,6 @@ var currentQuestion;
 var highScore = 0;
 var timeInterval;
 
-// click handler for save high score button
-function saveScore() {
-    // disable button after clicked so user cannot click a second time
-    ask.saveScore.disabled = true;
-
-    var initials = document.getElementById('initials').value;
-    var result = { initials: initials, score: score, when: Date.now() };
-    var saved = localStorage.getItem('scores');
-
-    // if there is no pre existing data then just save this result
-    if (saved === null) {
-        localStorage.setItem('scores', JSON.stringify([result]));
-        return;
-    }
-
-    // make an array of scores
-    var scores = JSON.parse(saved);
-    // append current result to array of scores
-    scores.push(result);
-    // sort (highest score, then by most recent)
-    scores.sort((a, b) => {
-        if (a.score > b.score) { return -1; }
-        if (a.score < b.score) { return 1; }
-
-        if (a.when > b.when) { return -1; }
-        if (a.when < b.when) { return 1; }
-
-        return 0;
-    });
-
-    localStorage.setItem('scores', JSON.stringify(scores));
-
-    toggleScore();
-}
-
 // start game when button is clicked 
 function quiz() {
     currentQuestion = 0;
@@ -133,6 +98,16 @@ const myQuestions = [
         question: "Which tool can you use to ensure code quality?",
         choices: ["Angular", "jQuery", "RequireJS", "ESLint"],
         answer: 3
+    },
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+        answer: 2
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Cascading Style Sheets", "Color Style Sync", "Computer Standard Style", "Cascading Sail Slides"],
+        answer: 0
     }
 ];
 
@@ -171,13 +146,48 @@ function onChoice(e) {
 // makes end section visible and other hide other sections
 function toggleEnd() {
     ask.saveScore.disabled = false;
-    el.showScore.textContent = "Score: " + score;
+    el.showScore.textContent = "Your score is: " + score;
     el.end.style.display = "flex";
     el.quiz.style.display = "none";
     el.defaultEl.style.display = "none";
     el.highScore.style.display = "none";
 
     clearInterval(timeInterval);
+}
+
+// click handler for save high score button
+function saveScore() {
+    // disable button after clicked so user cannot click a second time
+    ask.saveScore.disabled = true;
+
+    var initials = document.getElementById('initials').value;
+    var result = { initials: initials, score: score, when: Date.now() };
+    var saved = localStorage.getItem('scores');
+
+    // if there is no pre existing data then just save this result
+    if (saved === null) {
+        localStorage.setItem('scores', JSON.stringify([result]));
+        return;
+    }
+
+    // make an array of scores
+    var scores = JSON.parse(saved);
+    // append current result to array of scores
+    scores.push(result);
+    // sort (highest score, then by most recent)
+    scores.sort((a, b) => {
+        if (a.score > b.score) { return -1; }
+        if (a.score < b.score) { return 1; }
+
+        if (a.when > b.when) { return -1; }
+        if (a.when < b.when) { return 1; }
+
+        return 0;
+    });
+
+    localStorage.setItem('scores', JSON.stringify(scores));
+
+    toggleScore();
 }
 
 // makes score section visible and other hide other sections
@@ -190,12 +200,12 @@ function toggleScore() {
 
     var ol = document.querySelector('#highScore > ol');
     var scores = JSON.parse(localStorage.getItem('scores'));
-    
+
     scores.forEach(element => {
         var li = document.createElement('li');
         var text = document.createTextNode(element.initials + " " + element.score);
         li.appendChild(text);
         ol.appendChild(li);
-        
+
     });
 }
