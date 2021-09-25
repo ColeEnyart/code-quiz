d5baa263444d6e43e4a195a7b23b3a97f02de36f
Cole Enyart <letchro@gmail.com>
2021-09-24 22:26:30 -0500

    Added comments and reorganzined layout of functions for readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// selectors
 var el = {
     timerEl: document.getElementById('countdown'),
     defaultEl: document.getElementById('default'),
@@ -5,49 +6,56 @@ var el = {
     end: document.getElementById('results'),
     showScore: document.getElementById('showScore'),
     start: document.getElementById('startButton'),
-    playAgain: document.getElementById('playAgain'),
-    saveScore: document.querySelector('#saveScore button'),
     highScore: document.getElementById('highScore')
 }
 
+// select buttons
 var ask = {
     question: document.getElementById('question'),
     button1: document.getElementById('button1'),
     button2: document.getElementById('button2'),
     button3: document.getElementById('button3'),
-    button4: document.getElementById('button4')
+    button4: document.getElementById('button4'),
+    saveScore: document.querySelector('#saveScore button'),
+    playAgain: document.getElementById('playAgain')
 }
 
+// add click
 el.start.addEventListener('click', quiz, false);
-el.playAgain.addEventListener('click', quiz, false);
+ask.playAgain.addEventListener('click', quiz, false);
 ask.button1.addEventListener('click', onChoice, false);
 ask.button2.addEventListener('click', onChoice, false);
 ask.button3.addEventListener('click', onChoice, false);
 ask.button4.addEventListener('click', onChoice, false);
-el.saveScore.addEventListener('click', saveScore, false);
+ask.saveScore.addEventListener('click', saveScore, false);
 
-
+// global variables
 var timeLeft = 0;
 var score = 0;
 var currentQuestion;
 var highScore = 0;
 var timeInterval;
 
+// click handler for save high score button
 function saveScore() {
-    el.saveScore.disabled = true;
+    // disable button after clicked so user cannot click a second time
+    ask.saveScore.disabled = true;
 
     var initials = document.getElementById('initials').value;
     var result = { initials: initials, score: score, when: Date.now() };
     var saved = localStorage.getItem('scores');
 
+    // if there is no pre existing data then just save this result
     if (saved === null) {
-        saved = JSON.stringify([result]);
-        localStorage.setItem("scores", saved);
+        localStorage.setItem('scores', JSON.stringify([result]));
         return;
     }
 
+    // make an array of scores
     var scores = JSON.parse(saved);
+    // append current result to array of scores
     scores.push(result);
+    // sort (highest score, then by most recent)
     scores.sort((a, b) => {
         if (a.score > b.score) { return -1; }
         if (a.score < b.score) { return 1; }
@@ -58,12 +66,12 @@ function saveScore() {
         return 0;
     });
 
-    saved = JSON.stringify(scores);
-    localStorage.setItem("scores", saved);
+    localStorage.setItem('scores', JSON.stringify(scores));
 
     toggleScore();
 }
 
+// start game when button is clicked 
 function quiz() {
     currentQuestion = 0;
     score = 0;
@@ -72,15 +80,53 @@ function quiz() {
     askQuestion();
 }
 
+// start timer for quiz
+function startTimer() {
+    timeLeft = 21;
+    timeInterval = setInterval(timerTick, 1000);
+}
+
+// each escond that passes reduce amount of time left and check to see if user is out of time
+function timerTick() {
+    timeLeft--;
+    el.timerEl.textContent = "Time: " + timeLeft;
+
+    if (timeLeft <= 0) {
+        el.timerEl.textContent = "";
+        toggleEnd();
+    }
+
+}
+
+// makes quiz section visible and other hide other sections
+function toggleQuiz() {
+    el.quiz.style.display = "flex";
+    el.defaultEl.style.display = "none";
+    el.end.style.display = "none";
+    el.highScore.style.display = "none";
+
+}
+
+// sets up user interface to display question and choices
+function askQuestion() {
+    var current = myQuestions[currentQuestion];
+
+    ask.question.textContent = current.question;
+    ask.button1.textContent = current.choices[0];
+    ask.button2.textContent = current.choices[1];
+    ask.button3.textContent = current.choices[2];
+    ask.button4.textContent = current.choices[3];
+}
+
 const myQuestions = [
     {
         question: "Who invented JavaScript?",
-        choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich"],
+        choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich", "Bill Gates"],
         answer: 2
     },
     {
         question: "Which one of these is a JavaScript package manager?",
-        choices: ["Node.js", "TypeScript", "npm"],
+        choices: ["Node.js", "TypeScript", "npm", "Java"],
         answer: 2
     },
     {
@@ -90,6 +136,7 @@ const myQuestions = [
     }
 ];
 
+// compare button to what is clicked in choices array 
 function onChoice(e) {
     var chosen = -1;
 
@@ -121,26 +168,9 @@ function onChoice(e) {
     }
 }
 
-function askQuestion() {
-    var current = myQuestions[currentQuestion];
-
-    ask.question.textContent = current.question;
-    ask.button1.textContent = current.choices[0];
-    ask.button2.textContent = current.choices[1];
-    ask.button3.textContent = current.choices[2];
-    ask.button4.textContent = current.choices[3];
-}
-
-function toggleQuiz() {
-    el.quiz.style.display = "flex";
-    el.defaultEl.style.display = "none";
-    el.end.style.display = "none";
-    el.highScore.style.display = "none";
-
-}
-
+// makes end section visible and other hide other sections
 function toggleEnd() {
-    el.saveScore.disabled = false;
+    ask.saveScore.disabled = false;
     el.showScore.textContent = "Score: " + score;
     el.end.style.display = "flex";
     el.quiz.style.display = "none";
@@ -150,6 +180,8 @@ function toggleEnd() {
     clearInterval(timeInterval);
 }
 
+// makes score section visible and other hide other sections
+// displays highscores
 function toggleScore() {
     el.highScore.style.display = "flex";
     el.end.style.display = "none";
@@ -160,27 +192,10 @@ function toggleScore() {
     var scores = JSON.parse(localStorage.getItem('scores'));
     
     scores.forEach(element => {
-        var li = document.createElement("li");
+        var li = document.createElement('li');
         var text = document.createTextNode(element.initials + " " + element.score);
         li.appendChild(text);
         ol.appendChild(li);
         
     });
-}
-
-function startTimer() {
-    timeLeft = 21;
-    timeInterval = setInterval(timerTick, 1000);
-}
-
-
-function timerTick() {
-    timeLeft--;
-    el.timerEl.textContent = "Time: " + timeLeft;
-
-    if (timeLeft <= 0) {
-        el.timerEl.textContent = "";
-        toggleEnd();
-    }
-
 }
